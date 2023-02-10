dba6f7d36f33d84c860e8f5c6889abfc0eaf2aca
Immo Landwerth <immo@landwerth.net>
2023-02-10 14:27:20 -0800

    Initial commit

---
+++ src/test/Program.cs
@@ -0,0 +1,20 @@
+﻿var pullRequestNumber = 42;
+var pullRequestIsClosed = true;
+var pullRequestIsMerged = true;
+
+var githubOutputPath = Environment.GetEnvironmentVariable("GITHUB_OUTPUT");
+if (!string.IsNullOrEmpty(githubOutputPath))
+{
+    File.AppendAllLines(
+        githubOutputPath,
+        new[]{
+            $"pull_request_number={pullRequestNumber}",
+            $"pull_request_is_closed={pullRequestIsClosed.ToString().ToLower()}",
+            $"pull_request_is_merged={pullRequestIsMerged.ToString().ToLower()}"
+        }
+    );
+}
+
+// Console.WriteLine($"::set-output name=pull_request_number::{pullRequestNumber}");
+// Console.WriteLine($"::set-output name=pull_request_is_closed::{pullRequestIsClosed.ToString().ToLower()}");
+// Console.WriteLine($"::set-output name=pull_request_is_merged::{pullRequestIsMerged.ToString().ToLower()}");
